43c20cb70dc497f020e7d624cec9ce08c678644a
Peter Maselkowski <pmaselkowski@gmail.com>
2015-03-02 19:38:11 +0100

    *

--- src/Sitcom.php
+++ src/Sitcom.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This software package is licensed under New BSD license.
+ *
+ * @package maslosoft/sitcom
+ * @licence New BSD
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
+ * @copyright Copyright (c) Maslosoft
+ * @copyright Copyright (c) Others as mentioned in code
+ * @link http://maslosoft.com/mangan/
+ */
+
 namespace Maslosoft\Sitcom;
 
 class Sitcom

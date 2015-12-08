8fc1582f9be1f8579c88daa9743e9f51b845979c
Peter Maselkowski <pmaselkowski@gmail.com>
2015-12-08 10:00:38 +0100

    + Add config #1

--- src/Sitcom.php
+++ src/Sitcom.php
@@ -12,6 +12,7 @@
 
 namespace Maslosoft\Sitcom;
 
+use Maslosoft\Cli\Shared\ConfigReader;
 use Maslosoft\Signals\Signal;
 use Maslosoft\Sitcom\Helpers\CommandWrapper;
 use Symfony\Component\Console\Application;
@@ -20,16 +21,35 @@ class Sitcom
 {
 
 	/**
+	 * Config file name
+	 */
+	const ConfigName = "sitcom";
+
+	/**
 	 * Version number holder
 	 * @var string
 	 */
 	private static $_version = null;
 
+	public function __construct($configName = self::ConfigName)
+	{
+		$config = new ConfigReader($configName);
+		$cfg = (object) $config->toArray();
+		if (!empty($cfg->require))
+		{
+			$cwd = getcwd();
+			foreach ($cfg->require as $require)
+			{
+				require_once(sprintf('%s/%s', $cwd, $require));
+			}
+		}
+	}
+
 	public function addCommands(Application $app)
 	{
 		$signal = new Signal();
 		$signal->init();
-		$result = $signal->emit(new Command(new CommandWrapper($app)));
+		$signal->emit(new Command(new CommandWrapper($app)));
 	}
 
 	public function __get($name)
